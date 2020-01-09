f4fb62ff72c5957a8961eacfada564484cd3b76b
Saquib Irtiza <saquibirtiza@users.noreply.github.com>
2020-01-08 22:11:43 -0600

    Found the path

--- Pathfinder.py
+++ Pathfinder.py
@@ -15,8 +15,13 @@ sc = 0 #strating column number
 rq = queue.Queue(0) #empty row queue
 cq = queue.Queue(0) #empty column queue
 
+
 reached_end = False
 visited = np.zeros((5,7), dtype=int)
+parent = np.zeros((5,7), dtype=int)
+
+#hard coded for now
+parent[0][0] = 10000
 
 #north, south, east, west direction vectors
 dr = [-1, +1, 0, 0] 
@@ -26,6 +31,8 @@ dc = [0, 0, +1, -1]
 move_count = 0
 nodes_current_layer = 1
 nodes_next_layer = 0
+column = 0
+row = 0
 
 
 def explore_neighbours(r,c):
@@ -42,6 +49,7 @@ def explore_neighbours(r,c):
 
         rq.put(rr)
         cq.put(cc)
+        parent[rr][cc] = (r)*C + c
         visited[rr][cc] = 1
         nodes_next_layer += 1
 
@@ -51,15 +59,20 @@ def explore_neighbours(r,c):
 def solver():
     rq.put(sr)
     cq.put(sc)
+
     visited[sr][sc] = 1
     global nodes_current_layer
     global move_count
     global nodes_next_layer
+    global row
+    global column
 
     while rq.qsize() > 0:
         r = rq.get()
         c = cq.get()
         if m[r][c] == 'E':
+            row = r 
+            column = c
             reached_end = True
             break
         explore_neighbours(r,c)
@@ -74,6 +87,17 @@ def solver():
           
 
 output = solver()
-print(output)
+
+if (output != -1):
+    val = parent[row][column]
+    print("Parent Matrix: ")
+    print(parent)
+    print("Path:")
+    while val != 10000:
+        print("(",row,",", column,")")
+        column = val%C
+        row = int((val-column)/C)
+        val = parent[row][column]
+print ("( 0 , 0 )")
 
 
