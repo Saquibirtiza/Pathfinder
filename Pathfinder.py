e4ce653a5c5a417d1639dd8bd88104731d785228
abrar-labib <md.abrarlabib@gmail.com>
2020-01-10 00:30:45 -0600

    PyGame Grid

--- Pathfinder.py
+++ Pathfinder.py
@@ -1,103 +1,66 @@
-import numpy as np
-import queue
+import random, sys
+import pygame
 
-m = [['S',0,0,'B',0,0,0],
-    [0,'B',0,0,0,'B',0],
-    [0,'B',0,0,0,0,0],
-    [0,0,'B','B',0,0,0],
-    ['B',0,'B','E',0,'B',0]]
+screen = pygame.display.set_mode((750,850), 0, 32)
+screen.fill((255,255,255))
 
-R = 5 #Number of Rows 
-C = 7 #Number of Columns
+class Grid:
+    def __init__(self, width, height, num_colors=2):
+        self.wid = width
+        self.hei = height
+        # self.no_colors = num_colors
+        # self.color_list = [[None]*num_colors for n in range(num_colors)]
+        # self.color_list[0] = (238,233,233)
+        # self.color_list[1] = (238,213,183)
 
-sr = 0 #starting row number
-sc = 0 #strating column number
-rq = queue.Queue(0) #empty row queue
-cq = queue.Queue(0) #empty column queue
+        self.grid = [[0 for i in range(width)] for j in range(height)]
+
+    def show(self, screen):
+        for h in range(self.hei):
+            for w in range(self.wid):
+                x = 15*w
+                y = 100 + 15*h
+                block_hei = 15
+                block_wid = 15
+                
+                if self.grid[h][w] == 0:
+                    
+                    block_color = (238,213,183)
+                if self.grid[h][w] == 1:
+                    
+                    block_color = (238,233,233)
+                pygame.draw.rect(screen, block_color, (x,y,block_wid,block_hei))
+
+def draw_grid():
+    for x in range(0, 750, 15):
+        pygame.draw.line(screen, (0,0,0), (x, 100), (x, 850))
+    for y in range(100, 850, 15):
+        pygame.draw.line(screen, (0,0,0), (0, y), (750, y))
+
+def main():
+    pygame.init()
+    
+    mygrid = Grid(50,50)
+    mygrid.show(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if pygame.mouse.get_pressed()[0]:
+                # print(pygame.mouse.get_pos())
+                x_block = (pygame.mouse.get_pos()[0])//15
+                y_block = (pygame.mouse.get_pos()[1] - 100)//15
+                if(pygame.mouse.get_pos()[1] > 100):
+                    mygrid.grid[y_block][x_block] = 1
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        mygrid.show(screen)
+        draw_grid()
+        pygame.display.update()
 
 
-reached_end = False
-visited = np.zeros((5,7), dtype=int)
-parent = np.zeros((5,7), dtype=int)
-
-#hard coded for now
-parent[0][0] = 10000
-
-#north, south, east, west direction vectors
-dr = [-1, +1, 0, 0] 
-dc = [0, 0, +1, -1]
-
-#variables to track number of steps
-move_count = 0
-nodes_current_layer = 1
-nodes_next_layer = 0
-column = 0
-row = 0
-
-
-def explore_neighbours(r,c):
-    global nodes_next_layer
-    for i in range(0,4):
-        rr = r + dr[i]
-        cc = c + dc[i]
-
-        if rr<0 or cc < 0: continue
-        if rr >=R or cc >= C: continue
-
-        if visited[rr][cc]: continue
-        if m[rr][cc] == 'B': continue
-
-        rq.put(rr)
-        cq.put(cc)
-        parent[rr][cc] = (r)*C + c
-        visited[rr][cc] = 1
-        nodes_next_layer += 1
-
-
-
-
-def solver():
-    rq.put(sr)
-    cq.put(sc)
-
-    visited[sr][sc] = 1
-    global nodes_current_layer
-    global move_count
-    global nodes_next_layer
-    global row
-    global column
-
-    while rq.qsize() > 0:
-        r = rq.get()
-        c = cq.get()
-        if m[r][c] == 'E':
-            row = r 
-            column = c
-            reached_end = True
-            break
-        explore_neighbours(r,c)
-        nodes_current_layer -= 1
-        if nodes_current_layer == 0:
-            nodes_current_layer = nodes_next_layer
-            nodes_next_layer = 0
-            move_count += 1
-    if reached_end:
-        return move_count
-    return -1
-          
-
-output = solver()
-
-if (output != -1):
-    val = parent[row][column]
-    print("Parent Matrix: ")
-    print(parent)
-    print("Path:")
-    while val != 10000:
-        print("(",row,",", column,")")
-        column = val%C
-        row = int((val-column)/C)
-        val = parent[row][column]
-print ("( 0 , 0 )")
+main()
 
 
